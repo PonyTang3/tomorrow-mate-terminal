efa2d3f08000ec4f83d6e868951abab63ad20638
Alexander Dang-Tran <alex.dangtran@gmail.com>
2017-05-27 19:24:14 -0500

    Initial commit
    Took the solarized base configuration and the tomorrow theme mate script
    and combined.
    
    solarized: https://github.com/oz123/solarized-mate-terminal
    tomorrow: https://github.com/chriskempson/tomorrow-theme/blob/master/Mate-Terminal/setup-theme.sh

---
+++ README.md
@@ -0,0 +1,21 @@
+tomorrow-mate-terminal
+=======================
+
+tomorrow mate terminal
+
+## Usage
+
+Clone this repo and then run the script in the following way:
+
+    $ bash tomorrow-mate.sh
+
+Restart you terminal, enjoy a beatiful theme!
+
+The above will create a dark and light tomorrow themes.
+To remove these themes, run the script in the following
+way:
+
+    $ bash tomorrow-mate --reset
+
+Be warned though, this will remove all  custom themes, not
+just the ones installed by the script.
---
+++ tomorrow-mate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+which dconf 1>&2 > /dev/null || { echo "Please install dconf-cli"; exit 1; }
+
+if [[ $1 == "--reset" ]]; then
+   dconf reset -f /org/mate/terminal/global/profile-list
+   dconf reset -f /org/mate/terminal/profiles
+   exit 0
+fi
+# read profiles as string
+PROFILES=`dconf read /org/mate/terminal/global/profile-list`
+# add new profiles
+
+if [ -z "${PROFILES}" ]; then
+    PROFILES="['default','tomorrow-night']"
+elif [[ ${PROFILES} =~ 'tomorrow-night' ]]; then
+    echo "Found tomorrow-night in your themes, refusing to run twice"
+    echo "You can reset these setting with"
+    echo `basename "$0"` "--reset"
+    exit 1
+else
+    echo ${PROFILES}
+    M="'tomorrow-night']"
+    PROFILES=${PROFILES/]/, $M}
+fi
+
+dconf write /org/mate/terminal/global/profile-list "${PROFILES}"
+
+COLORS=`cat colors/palette`
+BGCOLOR=`cat colors/base03`
+BOLDCOLOR=`cat colors/base1`
+FGCOLOR=`cat colors/base0`
+
+PROFILE="tomorrow-night"
+# Do not use theme color
+dconf write /org/mate/terminal/profiles/${PROFILE}/use-theme-colors false
+
+# set palette
+dconf write /org/mate/terminal/profiles/${PROFILE}/palette \"$COLORS\"
+# set highlighted color to be different from foreground-color
+dconf write /org/mate/terminal/profiles/${PROFILE}/bold-color-same-as-fg false
+dconf write /org/mate/terminal/profiles/${PROFILE}/background-color \"$BGCOLOR\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/foreground-color \"$FGCOLOR\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/bold-color \"$BOLDCOLOR\"
+dconf write /org/mate/terminal/profiles/${PROFILE}/visible-name \"tomorrow-night\"
+
+echo -e "\e[37;41mTerminal must be restarted\e[0m"
